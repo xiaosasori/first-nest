3f389e91740ad9369832e2a2392461f7396033a7
Nguyen Minh Anh <xiaosasori@gmail.com>
2021-09-27 23:58:17 +0900

    custom data serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,8 +12,11 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('users')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
